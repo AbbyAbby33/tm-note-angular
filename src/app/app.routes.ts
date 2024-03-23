34a92484eb80788769a7ec19485b16785f121882
Abby <sk573129@gmail.com>
2024-03-24 01:00:26 +0800

    feat: add reuse layout template and adjust routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '@core/layout/layout.component';
 import { HomeComponent } from '@pages/general/home/home.component';
 import { LoginComponent } from '@pages/general/login/login.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'note', loadChildren: () => import('./pages/note/route').then(mod => mod.NOTE_ROUTES) },
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            { path: 'home', component: HomeComponent },
+            { path: 'note', loadChildren: () => import('./pages/note/route').then(mod => mod.NOTE_ROUTES) },
+        ]
+    },
     { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
