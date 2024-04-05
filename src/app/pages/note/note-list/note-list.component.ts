f77d40c127ab12fafa838542ee614ca84a14a0bb
Abby <sk573129@gmail.com>
2024-04-06 00:54:49 +0800

    feat: note list template

--- src/app/pages/note/note-list/note-list.component.ts
+++ src/app/pages/note/note-list/note-list.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-note-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './note-list.component.html',
-  styleUrl: './note-list.component.scss'
+    selector: 'app-note-list',
+    standalone: true,
+    imports: [],
+    templateUrl: './note-list.component.html',
+    styleUrl: './note-list.component.scss'
 })
 export class NoteListComponent {
+    menuList = [
+        {
+            name: '前端',
+            list: ['Angular']
+        },
+        {
+            name: '後端',
+            list: ['Node.js', 'C#']
+        }
+    ];
 
+    bookTitle = 'Angular';
+
+    bookMenuList = [
+        {
+            subTitle: '基礎',
+            list: [
+                { title: 'Route', category: 'principle' },
+                { title: 'Guard', category: 'principle' },
+                { title: 'Standalone Project', category: 'principle' }
+            ]
+        },
+        {
+            subTitle: '狀態管理：Ngxs',
+            list: [
+                { title: 'Ngxs簡介', category: 'principle' },]
+        }
+    ];
 }
